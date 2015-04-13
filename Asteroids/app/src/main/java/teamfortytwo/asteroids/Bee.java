70da55f67f9ae3cea6e6032b4a5a3c72a13ef109
bwebster4 <bwebster496@gmail.com>
2015-04-13 17:57:19 -0400

    Began adding Bee
    added a class for bee, and relevant drawable xml file. still very much
    a work in progress

---
+++ Asteroids/app/src/main/java/teamfortytwo/asteroids/Bee.java
@@ -0,0 +1,18 @@
+package teamfortytwo.asteroids;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+/**
+ * Created by BrandonWebster on 4/13/15.
+ */
+public class Bee extends ImageView{
+
+
+    public Bee(Context context) {
+        super(context);
+    }
+}
